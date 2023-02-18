1236057debb91ff4c1e5d27363c1495996301a3d
titivince <72084738+titivince@users.noreply.github.com>
2023-02-18 14:25:51 +0100

    burger
    creating the burger action

--- asset/script.js
+++ asset/script.js
@@ -1,9 +1,11 @@
 window.onload = function() { /* start when html is ready */
+var menu;
 
-var btn0 = document.getElementById("nav-0");
+var nav = document.getElementById("nav");
 var btn1 = document.getElementById("nav-1");
 var btn2 = document.getElementById("nav-2");
 var btn3 = document.getElementById("nav-3");
+var burger = document.getElementById("burger");
 
 var topSection = document.getElementById("topSection");
 var midleSection = document.getElementById("midleSection");
@@ -12,25 +14,41 @@ var bottomSection = document.getElementById("bottomSection");
 var reservation = document.getElementById("reservation");
 var response = document.getElementById("response");
 
+function burgerOpen(){
+    if (menu == true) {
+        menu = false;
+        burger.classList = "";
+        nav.classList.remove("inline");
+        nav.classList.remove("load");
+        nav.classList.add("d-none");
+    } else {
+        menu = true;
+        nav.classList.remove("d-none");
+        nav.classList.add("load");
+        nav.classList.add("inline");
+        burger.classList = "";
+        burger.classList.add("open");
+    }
+}
 function reserved(){
     response.classList = "";
-    response.className = "inline";
+    response.classList.add("inline");
 }
 
-function topPage(){
-    window.scrollTo({top: 0, behavior: 'smooth'});
-}
 function scrollToTopSection(){
     topSection.scrollIntoView({behavior: 'smooth'});
+    burgerOpen();
 }
 function scrollToMidleSection(){
     midleSection.scrollIntoView({behavior: 'smooth'});
+    burgerOpen();
 }
 function scrollToBottomSection(){
     bottomSection.scrollIntoView({behavior: 'smooth'});
+    burgerOpen();
 }
+burger.addEventListener("click", burgerOpen);
 
-btn0.addEventListener("click", topPage);
 btn1.addEventListener("click", scrollToTopSection);
 btn2.addEventListener("click", scrollToMidleSection);
 btn3.addEventListener("click", scrollToBottomSection);
