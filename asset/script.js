ea2ab270193995df55dbdefe979d7af5983dfbd4
titivince <72084738+titivince@users.noreply.github.com>
2023-02-27 11:05:52 +0100

    Style
    add style wanted by the client

--- asset/script.js
+++ asset/script.js
@@ -5,11 +5,12 @@ var nav = document.getElementById("nav");
 var btn1 = document.getElementById("nav-1");
 var btn2 = document.getElementById("nav-2");
 var btn3 = document.getElementById("nav-3");
+var btn4 = document.getElementById("nav-4");
 var burger = document.getElementById("burger");
 
 var topSection = document.getElementById("topSection");
 var midleSection = document.getElementById("midleSection");
-var bottomSection = document.getElementById("bottomSection");   
+var bottomSection = document.getElementById("bottomSection");
 
 var reservation = document.getElementById("reservation");
 var response = document.getElementById("response");
@@ -18,21 +19,18 @@ function burgerOpen(){
     if (menu == true) {
         menu = false;
         burger.classList = "";
-        nav.classList.remove("inline");
         nav.classList.remove("load");
         nav.classList.add("d-none");
     } else {
         menu = true;
         nav.classList.remove("d-none");
         nav.classList.add("load");
-        nav.classList.add("inline");
         burger.classList = "";
         burger.classList.add("open");
     }
 }
 function reserved(){
     response.classList = "";
-    response.classList.add("inline");
 }
 
 function scrollToTopSection(){
@@ -47,11 +45,16 @@ function scrollToBottomSection(){
     bottomSection.scrollIntoView({behavior: 'smooth'});
     burgerOpen();
 }
+function scrollToFooter(){
+    response.scrollIntoView({behavior: 'smooth'});
+    burgerOpen();
+}
 burger.addEventListener("click", burgerOpen);
 
 btn1.addEventListener("click", scrollToTopSection);
 btn2.addEventListener("click", scrollToMidleSection);
 btn3.addEventListener("click", scrollToBottomSection);
+btn4.addEventListener("click", scrollToFooter);
 
 reservation.addEventListener("click", reserved);
 }
